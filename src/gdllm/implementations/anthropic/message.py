071705b0717c4e62760b861b771d9d7b0a6a4087
GandalfsDad <rhianmcclelland193@hotmail.com>
2025-02-26 07:28:17 +1000

    Add Anthropic reasoning compatability
    * Uprev to 0.2.0a0
    
    * Update anthropic dependency
    
    * Add Anthropic Reasoning functionality

--- src/gdllm/implementations/anthropic/message.py
+++ src/gdllm/implementations/anthropic/message.py
@@ -28,6 +28,28 @@ class AnthropicResponse(AbstractAnthropicMessage):
     
     def print(self):
         print("Role: assistant\nContent: " + self.response.text)
+
+class AnthropicReasoningResponse(AbstractAnthropicMessage):
+    def __init__(self, response):
+        self.response = response
+
+    def to_chat_message(self) -> dict:
+        return {"role": "assistant", 
+                "content": [{"type":"thinking", "thinking": content.thinking, "signature": content.signature} if content.type == "thinking" else 
+                            {"type":"redacted_thinking", "data": content.data} if content.type == "redacted_thinking" else
+                            {"type":"text", "text": content.text} for content in self.response.content]
+                }
+    
+    def print(self):
+        for content in self.response.content:
+            if content.type == "text":
+                print("Role: assistant\nContent: " + content.text)
+            elif content.type == "thinking":
+                print("Role: assistant\nThinking: " + content.thinking)
+            elif content.type == "redacted_thinking":
+                print("Role: assistant\nRedacted thinking!")
+            else:
+                raise Exception("Unknown content type")
     
 class AnthropicToolResponse(AbstractAnthropicMessage):
     def __init__(self, response):
@@ -35,22 +57,27 @@ class AnthropicToolResponse(AbstractAnthropicMessage):
     
     def to_chat_message(self) -> dict:
         return {"role": "assistant", 
-                "content": [
-                    {
-                        "type":"text",
-                        "text": self.response.content[0].text
-                    },
-                    {
-                        "type":"tool_use",
-                        "id": self.response.content[1].id,
-                        "name": self.response.content[1].name,
-                        "input": self.response.content[1].input
-                    }
-                ]
+                "content": 
+                    [ {"type":"text", "text": content.text} if content.type == "text" else 
+                     {"type":"thinking", "thinking": content.thinking, "signature": content.signature} if content.type == "thinking" else
+                     {"type":"redacted_thinking", "data": content.data} if content.type == "redacted_thinking" else
+                     {"type":"tool_use", "id": content.id, "name": content.name, "input": content.input} 
+                     for content in self.response.content
+                    ]
                 }
     
     def print(self):
-        print(f"Role: assistant\nContent: {self.response.content[0].text}\nTool use: {str(self.response.content[1])}")
+        for content in self.response.content:
+            if content.type == "text":
+                print("Role: assistant\nContent: " + content.text)
+            elif content.type == "thinking":
+                print("Role: assistant\nThinking: " + content.thinking)
+            elif content.type == "redacted_thinking":
+                print("Role: assistant\nRedacted thinking!")
+            elif content.type == "tool_use":
+                print("Role: assistant\nTool use: " + str(content))
+            else:
+                raise Exception("Unknown content type")
 
 class AnthropicToolResultResponse(AbstractAnthropicMessage):
     def __init__(self, id, result):
